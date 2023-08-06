a7d44af55e2d55513dc5dc7bd8b390d5ba2a6323
albeyeren <albeyeren.gameonly@gmail.com>
2023-08-06 15:32:32 +0700

    game

--- game.php
+++ game.php
@@ -5,17 +5,17 @@ while (true) {
     echo "===== Game tebak angka =====\n";
     echo "Tebak sebuah angka antara 1 sampai 9!\n";
 
-    $computer = rand(1, 9);
+    $computer = rand(1, 9); // Angka random dari computer yang akan ditebak
 
     echo "Masukan tebakanmu: ";
-    $player = trim(fgets(STDIN));
+    $player = trim(fgets(STDIN)); // menerima input dari player/user
 
     if ($player == $computer) {
-        echo "Tebakanmu benar!\n";
+        echo "Tebakanmu benar!\n"; //jika tebakan mu benar
         exit;
     } elseif ($player == 0 || $player > 9) {
-        echo "Kamu hanya boleh memasukan angka 1 - 9!\n";
+        echo "Kamu hanya boleh memasukan angka 1 - 9!\n"; //Sesi ini akan memvalidasi bahwah hanya angka 1 - 9 yang boleh dimasukan.
     } else {
-        echo "Tebakanmu salah! Angka komputer adalah $computer\n";
+        echo "Tebakanmu salah! Angka komputer adalah $computer\n"; //Jika tebakan salah
     }
 }
